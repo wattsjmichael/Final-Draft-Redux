6bf19457b9896c39fa54a4de03d35f3a74325471
Michael Watts <wattsjmichael@gmail.com>
2020-12-04 12:18:41 -0800

    Create toggle form visiblity meaningful fail test for kegformreducer

--- src/_tests_/reducers/keg-form-visible-reducer.test.js
+++ src/_tests_/reducers/keg-form-visible-reducer.test.js
@@ -1,8 +1,14 @@
 import kegFormVisibleReducer from '../../reducers/keg-form-visible-reducer';
 
 describe ("kegFormVisibleReducer", ()=> {
+  
   test ('Should return default state if no action type is recognized', ()=> {
     expect(kegFormVisibleReducer(false, { type: null})).toEqual(false);
     
-  })
+  });
+
+  test ('Should toggle form visiblity to true', () => {
+    expect(kegFormVisibleReducer(false, { type: 'TOGGLE_FORM'})).toEqual(true);
+  });
+  
 })
