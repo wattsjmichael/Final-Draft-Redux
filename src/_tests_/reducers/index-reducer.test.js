44b71b793d074a2dea59ac4191cfd2a48eef41ed
Michael Watts <wattsjmichael@gmail.com>
2020-12-04 12:35:10 -0800

    Sanity check for draftListReducer to make sure that the default state matches the state slice of the root reducer

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,10 +1,21 @@
 import  rootReducer from '../../reducers/index';
+import { createStore } from 'Redux';
+import kegFormVisibleReducer from '../../reducers/keg-form-visible-reducer';
+import draftListReducer from '../../reducers/draft-list-reducer';
+
+let store = createStore(rootReducer)
 
 describe ("rootReducer", ()=> {
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, {type:null})).toEqual({
       fullDraftList: {},
       kegFormVisibleOnPage: false
-    })
-  })
+    });
+  });
+
+test ('Check that initial state of draftListReducer mathces root reducer', () => {
+ expect(store.getState().fullDraftList).toEqual(draftListReducer(undefined, {type: null}));
+});
+
+
 })
