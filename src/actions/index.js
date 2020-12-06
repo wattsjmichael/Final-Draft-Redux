65b7f531db2cff03a5373e98b54c0055e86559bf
Michael Watts <wattsjmichael@gmail.com>
2020-12-06 12:26:44 -0800

    adds passing test for action ADD_KEG

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,21 @@
 export const deleteKeg = id => ({
   type: 'DELETE_KEG',
   id
-})
+});
 
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
-})
+});
+
+export const addKeg = (keg) => {
+  const {name, brand, abv, price, pintsLeft, id} = keg;
+  return {
+    type: 'ADD_KEG',
+    name: name,
+    brand: brand,
+    abv: abv,
+    price: price,
+    pintsLeft: pintsLeft,
+    id: id
+  }
+}
