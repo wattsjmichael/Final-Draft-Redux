59101097b5173585140106e70e5fa65f5a062769
Michael Watts <wattsjmichael@gmail.com>
2020-12-06 13:59:53 -0800

    Adds Meaningfull fail for selecting an ID in selectedKegReducer

--- src/_tests_/reducers/selected-keg-reducer.test.js
+++ src/_tests_/reducers/selected-keg-reducer.test.js
@@ -1,7 +1,42 @@
 import selectedKeg from "../../reducers/selected-keg-reducer";
 
-describe ("selectedKegReducer", () => {
+
+
+describe("selectedKegReducer", () => {
+
+  const fullDraftList = [{
+    name: "BCB",
+    brand: "BCB",
+    price: "4.25",
+    abv: "2.3",
+    pintsLeft: 123,
+    id: "1",
+  },{
+    name: "the abyss",
+    brand: "deschutes",
+    price: "10.00",
+    abv: "12.5",
+    pintsLeft: 43,
+    id: "2"
+  }];
+
   test('Should return default state if no action type is recognized', () => {
-  expect(selectedKeg(undefined, {type:null})).toEqual(null);
-});
+    expect(selectedKeg(undefined, { type: null })).toEqual(null);
+  });
+
+  test("selects keg by Id", () => {
+    const action = {
+      type: 'SELECT_KEG',
+      id: '2'
+    }
+    expect(selectedKeg(fullDraftList, action)).toEqual({
+      name: "the abyss",
+      brand: "deschutes",
+      price: "10.00",
+      abv: "12.5",
+      pintsLeft: 43,
+      id: "2"
+    })
+  })
+
 });
